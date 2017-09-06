d2f0fdfd816b21326cd68ee10bcf1054f0e7f456
jberron201605 <jberron201605@gmail.com>
2017-09-06 08:34:07 -0400

    modified:   twoFifty.php

--- twoFifty.php
+++ twoFifty.php
@@ -7,7 +7,7 @@ $found = false;
 
 if (!isset($missingNumber))
 {
-  $missingNumber = rand() % 250;
+  $missingNumber = rand() % 251;
 }
  
 // ******** Prepare Strings ******
@@ -39,31 +39,31 @@ for ($d=0;$d<=9;$d++)
 
 if ($missingDigitsCount == 1) 
 {
-  echo "Solved because it is a single digit number: ". $candidateNumber . '<br>';
+  echo "Solved because it is a single digit number: ". $candidateNumber . '<br><br>';
   $found = true;
 }
 
 if ($missingDigitsCount == 2)
 {
   $permutations[0] = $candidateNumber;
-  $permutations[1] = str_split($candidateNumber)[1] . str_split($candidateNumber)[0]; //Just interchange the digits
+  $permutations[1] = $candidateNumber[1] . $candidateNumber[0]; //Just interchange the digits
 }
 
 if ($missingDigitsCount == 3) // This is where it gets interesting as we should use all the permutations for the 3 digits which are 6 different permutations, I will just mix them directly instead of writing a function for simplicity
 {
-  $permutations[0] = $candidateNumber; // or in other words  str_split($candidateNumber)[0] . str_split($candidateNumber)[1] . str_split($candidateNumber)[2};
-  $permutations[1] = str_split($candidateNumber)[0] . str_split($candidateNumber)[2] . str_split($candidateNumber)[1]; 
-  $permutations[2] = str_split($candidateNumber)[1] . str_split($candidateNumber)[0] . str_split($candidateNumber)[2]; 
-  $permutations[3] = str_split($candidateNumber)[1] . str_split($candidateNumber)[2] . str_split($candidateNumber)[0]; 
-  $permutations[4] = str_split($candidateNumber)[2] . str_split($candidateNumber)[0] . str_split($candidateNumber)[1]; 
-  $permutations[5] = str_split($candidateNumber)[2] . str_split($candidateNumber)[1] . str_split($candidateNumber)[0]; 
+  $permutations[0] = $candidateNumber;
+  $permutations[1] = $candidateNumber[0] . $candidateNumber[2] . $candidateNumber[1]; 
+  $permutations[2] = $candidateNumber[1] . $candidateNumber[0] . $candidateNumber[2]; 
+  $permutations[3] = $candidateNumber[1] . $candidateNumber[2] . $candidateNumber[0]; 
+  $permutations[4] = $candidateNumber[2] . $candidateNumber[0] . $candidateNumber[1]; 
+  $permutations[5] = $candidateNumber[2] . $candidateNumber[1] . $candidateNumber[0]; 
 }
 
 //cleanup the permutations we dont want anything that starts with zero or that is higher than our max
-
+$permutations = array_unique($permutations);
 foreach ($permutations as $p => $permutation)                                                                                
 {
- if ((str_split($permutations[$p])[0] == 0) || ((int)$permutations[$p] > (int)$maxNumber)) 
+ if (($permutations[$p][0] == 0) || ((int)$permutations[$p] > (int)$maxNumber)) 
   {
     unset($permutations[$p]);
   }
@@ -74,7 +74,7 @@ reset ($permutations);
                                                                                   
 if (count($permutations) == 1) 
 {
-  echo "Solved because we have only one valid permutation: ". current($permutations) . '<br>';
+  echo "Solved because we have only one valid permutation: ". current($permutations) . '<br><br>';
   $found = true;
 }
 
@@ -82,17 +82,30 @@ if (!$found)
 {
   foreach ($permutations as $permutation)
   {
-    if ((int)$permutation <= 50 && substr_count($randomString, $permutation) < 3) { $found = true; echo 'The missing number is: '. $permutation. ' because there is less than three occurrences and the number is less or equal than 50 <br>'; break;}
-    if ((int)$permutation > 50 and (int)$permutation < 100  && substr_count($randomString, $permutation) < 2) { $found = true; echo 'The missing number is: ' . $permutation. ' because there is less than two occurrences and the number is between 50 and 100<br>'; break;}
-    if ((int)$permutation >= 100 && (int)$permutation <= $maxNumber && substr_count($randomString, $permutation) < 1) { $found = true; echo 'The missing number is: ' . $permutation. ' because there is no occurrences and the number is greater than 100<br>'; break;}
+    if ((int)$permutation <= 50 && substr_count($randomString, $permutation) < 3) { $found = true; echo 'The missing number is: '. $permutation. ' because there is less than three occurrences and the number is less or equal than 50 <br><br>'; break;}
+    if ((int)$permutation > 50 and (int)$permutation < 100  && substr_count($randomString, $permutation) < 2) { $found = true; echo 'The missing number is: ' . $permutation. ' because there is less than two occurrences and the number is between 50 and 100<br><br>'; break;}
+    if ((int)$permutation >= 100 && (int)$permutation <= $maxNumber && substr_count($randomString, $permutation) < 1) { $found = true; echo 'The missing number is: ' . $permutation. ' because there are no occurrences and the number is greater or equal than 100<br><br>'; break;}
   }      
 }
 
-if (!$found)   {echo 'It was not possible to find the number using the current method.' .'<br>' ;}                                                                                                                                                         
+if (!$found)   
+{
+
+  echo 'It was not possible to find the exact number using the current method.' .'<br><BR>' ;
+  echo 'The candidates we have for a missing number are: <br>';
+  foreach ($permutations as $key => $permutation)
+  {
+    echo $permutation . ' Times found: ' . (substr_count($randomString, $permutation)) . '<br>';
+  }
+  echo '<br>';
+}                                                                                                                                                         
                                                                                   
-echo '$missingNumber: '. $missingNumber. '<br> <br>';
-echo $randomString . '<br> <br>';
+echo 'MISSING NUMBER:  <BR>'. $missingNumber. '<br> <br>';
+echo 'FULL STRING:'.'<br>';
 echo $expectedString . '<br> <br>';
-echo 'missingDigits: '. $missingDigitsCount . '<br> <br>';
+echo 'IMCOMPLETE STRING:'.'<br>';
+echo $randomString . '<br> <br>';
+
+echo 'MISSING DIGITS: <BR> '. $missingDigitsCount . '<br> <br>';
 echo 'CANDIDATES:'.'<br> <br>';
 var_dump($permutations);
